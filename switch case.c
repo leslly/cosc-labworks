3b2450c6c7fe10b4f533ff369f04be7a021d8a0f
Akinrinde Leslie Teniola <87911912+leslly@users.noreply.github.com>
2021-10-09 02:24:52 +0100

    Add files via upload

---
+++ switch case.c
@@ -0,0 +1,36 @@
+ #include<stdio.h>
+int main()
+{
+int Grade
+printf("each 1, 2, 3, 4, 5 represents the price of silk");
+scanf("%d", &Grade);
+switch(Grade){
+	case 1:
+		printf("1 is %d", 100000);
+		break;
+		case 2:
+			printf("2 is %d",250000);
+			break;
+			case 3:
+				prinf("3 is %d", 350000);
+				break;
+				case 4:
+					printf("4 is %d",500000);
+					break;
+					case 5:
+						printf("5 is %d",620000);
+						break;
+		
+		
+		
+		
+		
+		
+		
+}
+	
+	
+	
+	
+	
+}
